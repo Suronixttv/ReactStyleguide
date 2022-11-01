6275f3bfeef3b7cf7ea27b2c3dc92667191dbe7b
Suronix <anas.lemkaddem@gmail.com>
2022-11-01 20:05:45 +0100

    Inital Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-gray">
+      <div className="h-16 flex items-center">
+        <p>Main App System</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
